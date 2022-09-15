77398a8f0d313f4f30865a570b0f7309f6f6af4d
Justina Mandaro <bautistajustina7@gmail.com>
2022-09-15 13:12:25 -0700

    commit tree 38dbc241d3a9207dca9e63224ae5ba357e97f6da

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Add your code here
+const formData ={
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+function submitData() {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),})
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          console.log(object);
+          renderUser(object)
+        })
+
+    .catch(function (error) {
+        const error1 = document.createElement('div');
+
+        const errMessage = document.createElement('p');
+          errMessage.textContent = //'message: Unauthorized Access, code: 401'
+          `message: ${error.message}
+          code: ${error.code}`
+          
+          error1.append(errMessage);
+          document.body.appendChild(error1);
+          
+        });  
+        
+    
+
+    }
+        function renderUser(object){
+            const user = document.createElement('div');
+            
+            const listUser = document.createElement('p');
+            listUser.id = `${object.id}`;
+            listUser.textContent = `
+            Name: ${object.name}
+            Email: ${object.email}
+            ID:  ${object.id}
+            `
+            
+            user.append(listUser);
+            document.body.appendChild(user);
+        }
+
+    
